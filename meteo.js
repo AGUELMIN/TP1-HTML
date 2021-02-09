2c411d72d8139769fb69748f15b8b2745c404b02
AGUELMIN <78732107+AGUELMIN@users.noreply.github.com>
2021-02-09 11:22:34 +0100

    Update meteo.js

--- meteo.js
+++ meteo.js
@@ -1,1 +1,31 @@
-﻿
+<script type="text/javascript">
+			
+			
+		    function showCity(vs){
+		        let villeCourante = "LYON";
+		        console.log(villeCourante);
+		        
+		        var vs = document.getElementById(vs);
+	
+		        
+					if (vs.value == "Lyon") { 
+						Lyon.style.display= "";
+						Paris.style.display= "none"; 
+						Bejaia.style.display= "none";  
+
+					}
+					if (vs.value == "Paris") { 
+					    Lyon.style.display= "none";
+					    Paris.style.display= ""; 
+					    Bejaia.style.display= "none"; 
+					}
+					if (vs.value == "Bejaia") { 
+					    Lyon.style.display= "none";
+					    Paris.style.display= "none"; 
+					    Bejaia.style.display= ""; 
+					}
+				
+					
+				
+			}
+		</script> 
